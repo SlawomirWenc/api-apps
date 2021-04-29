82f51ddafc0fe5a067ef5f2edb03f790eb5dc0cf
SlawomirWenc <slawomirwenc@gmail.com>
2021-04-29 13:40:37 +0200

    Initial commit

---
+++ src/main/java/com/example/apiapps/ApiAppsApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAppsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAppsApplication.class, args);
+	}
+
+}
